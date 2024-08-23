21c4bff2aafc535f530e4578b4fee357b8b949e5
5four0 <wang.40.pat@gmail.com>
2024-08-23 10:26:52 +0800

    0823

--- assets/kiks-dev-jerome.js
+++ assets/kiks-dev-jerome.js
@@ -74,12 +74,32 @@ if (document.body.classList.contains('template-product')) {
     // console.log('Body does not have the class "template-product".');
 }
 
+// const mediaWithTextSection = document.querySelector(".cc-media-with-text");
+
+// if (mediaWithTextSection) {
+//   const stringWithSpaces = document.querySelectorAll(".cc-media-with-text .section__block.reading-width.rte p");
+
+//   stringWithSpaces.forEach(element => {
+//     const textWithoutNonBreakingSpaces = element.innerHTML.replace(/&nbsp;/g, ' ');
+//     element.innerHTML = textWithoutNonBreakingSpaces;
+//   });
+// }
 const mediaWithTextSection = document.querySelector(".cc-media-with-text");
+const faqSection = document.querySelector(".cc-faq");
 
 if (mediaWithTextSection) {
   const stringWithSpaces = document.querySelectorAll(".cc-media-with-text .section__block.reading-width.rte p");
 
   stringWithSpaces.forEach(element => {
+    const textWithoutNonBreakingSpaces = element.innerHTML.replace(/&nbsp;/g, ' ');
+    element.innerHTML = textWithoutNonBreakingSpaces;
+  });
+}
+
+if (faqSection) {
+  const faqStringsWithSpaces = document.querySelectorAll(".cc-faq details.disclosure .disclosure__panel .disclosure__content.rte p");
+
+  faqStringsWithSpaces.forEach(element => {
     const textWithoutNonBreakingSpaces = element.innerHTML.replace(/&nbsp;/g, ' ');
     element.innerHTML = textWithoutNonBreakingSpaces;
   });
